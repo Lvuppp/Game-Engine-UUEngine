169975882ac4798b03cfd01ad9fd2d2da2faa1ff
# <egor.bagrovets.eb@gmail.com>
2025-05-09 12:29:18 +0200

    feat: create base folder class

--- src/Core/Folders/BaseFolder.cpp
+++ src/Core/Folders/BaseFolder.cpp
@@ -1,16 +1,32 @@
 #include "BaseFolder.h"
+#include <algorithm>
 
 void cBaseFolder::append(uint32_t hash, const std::string &fileName)
 {
-    m_fileFolder.insert(std::make_pair<>(hash, fileName));
+    m_files.insert(std::make_pair<>(hash, fileName));
 }
 
 void cBaseFolder::remove(uint32_t hash)
 {
-    m_fileFolder.erase(hash);
+    m_files.erase(hash);
 }
 
 void cBaseFolder::replace(uint32_t hash, const std::string &fileName)
 {
-    m_fileFolder[hash] = fileName;
+    m_files[hash] = fileName;
+}
+
+std::string_view cBaseFolder::getFile(uint32_t hash)
+{
+    return m_files[hash].data();
+}
+
+std::vector<std::string_view> cBaseFolder::getFiles() const
+{
+    std::vector<std::string_view> files;
+    for (const auto file : m_files)
+    {
+        files.push_back(file.second.data());
+    }
+    return files;
 }
--- src/Core/Folders/BaseFolder.h
+++ src/Core/Folders/BaseFolder.h
@@ -2,16 +2,23 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 #include <unordered_map>
-
 
 class cBaseFolder
 {
 public:
+    cBaseFolder() = default;
+
+    cBaseFolder(const cBaseFolder&) = delete;
+    cBaseFolder& operator=(const cBaseFolder&) = delete;
+
     void append(uint32_t hash, const std::string &fileName);
     void remove(uint32_t hash);
     void replace(uint32_t hash, const std::string &fileName);
+    std::string_view getFile(uint32_t hash);
+    std::vector<std::string_view> getFiles() const;
 
 protected:
-    std::unordered_map<uint32_t, std::string>  m_fileFolder;
+    std::unordered_map<uint32_t, std::string>  m_files;
 };
